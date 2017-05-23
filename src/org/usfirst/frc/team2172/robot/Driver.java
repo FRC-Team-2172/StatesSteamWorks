3547dbce9846201d673d52588e8a916ea0ed4299
Alex W <mastereverythings@gmail.com>
2017-05-23 15:56:41 -0400

    Fix?

--- src/org/usfirst/frc/team2172/robot/Driver.java
+++ src/org/usfirst/frc/team2172/robot/Driver.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc.team2172.robot;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.FeedbackDevice;
 
@@ -9,6 +13,17 @@ public class Driver extends RobotDrive{
 	private CANTalon drive2;
 	private CANTalon drive3;
 	private CANTalon drive4;
+	private ADXRS450_Gyro gyro;
+	private Timer timer;
+	private static double x = 0;
+	private static double y = 0;
+	private static double displacement = 0;
+	public static double displacement2;
+	private static boolean gearAligned;
+	public static boolean hasAutoRun = false;
+	private double leftSpeed;
+	private double rightSpeed;
+	
 	
 	public Driver(CANTalon canTalon1, CANTalon canTalon2, CANTalon canTalon3, CANTalon canTalon4) {
 		super(canTalon1, canTalon4, canTalon2, canTalon3);
@@ -70,5 +85,69 @@ public class Driver extends RobotDrive{
 	
 	public void goTo(Point point, Navigator navigator){
 		Point[] path = navigator.generatePath(point, Math.PI/4);
+	}
+	public void alignGearPeg(){
+		boolean gearAligned = false;
+		
+		
+	}
+	public double distance(double x){
+		double velocity = (drive1.getSpeed()-drive2.getSpeed())/2.0;
+	    x = x + velocity * 0.01;
+		return velocity;
+	}
+	public static void encoderThread(CANTalon driv1, CANTalon driv2){
+		displacement2 = 0.0;
+		Thread t1 = new Thread(() -> {
+			//gyro.reset();
+	        while (!Thread.interrupted()) {
+	        	//timer.reset();
+	    		//double speed = (driv1.getEncVelocity() - driv2.getEncVelocity())/2.0*0.82*3.1415926/210.0;
+	    		//x = x + speed * Math.cos(gyro.getAngle()%360 * 3.14 / 180.0) * 0.01;//timez.get();
+	    		//y = y + speed * Math.sin(gyro.getAngle()%360 * 3.14 / 180.0) * 0.01;//times.get();
+	    		//displacement = Math.sqrt(Math.pow(y,2) +Math.pow(x,2));
+	    		displacement2 = displacement2 + (((Math.abs(driv1.getEncVelocity()) + Math.abs(driv2.getEncVelocity())/2.0)*0.01)/210);
+	    		Timer.delay(0.01);
+	        }
+	    });
+	    t1.start();
+	    
+	}
+	public double displacement(){
+		return displacement;
+	}
+	public boolean gearAligned(){
+		return gearAligned;
+	}
+	public void straightDrive(double speed){
+		if (hasAutoRun == false){
+			leftSpeed = speed * 0.9;
+			rightSpeed = speed * 0.9;
+			speed = speed *0.9;
+			this.drive1.set(leftSpeed);
+			this.drive2.set(rightSpeed);
+			this.drive3.set(-speed);
+			this.drive4.set(-speed);
+		}
+		hasAutoRun = true;
+		/*if (leftSpeed + rightSpeed > (2.0 * speed)){
+			if (drive1.getEncVelocity() - 10.0 > drive2.getEncVelocity()){
+				leftSpeed = leftSpeed - 0.005;
+				this.drive1.set(leftSpeed);
+			}else if (drive1.getEncVelocity() + 10.0 < drive2.getEncVelocity()){
+				rightSpeed = rightSpeed - 0.005;
+				this.drive2.set(rightSpeed);
+			}
+		}else{*/
+			if (drive1.getEncVelocity() - 5.0 > drive2.getEncVelocity()){
+				rightSpeed = rightSpeed + 0.005;
+				this.drive2.set(rightSpeed);
+			}else if (drive1.getEncVelocity() + 5.0 < drive2.getEncVelocity()){
+				leftSpeed = leftSpeed + 0.005;
+				this.drive1.set(leftSpeed);
+			}
+		//}
+		this.drive1.set(leftSpeed);
+		this.drive2.set(rightSpeed);
 	}
 }
